64b13167fbec4dd04ba8ce685cfd1b990df995cd
unit02 <elizaorchard@gmail.com>
2015-08-04 14:30:24 +1200

    Uploaded initial workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/eliza/306project/src
+# Build directory: /home/eliza/306project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/eliza/306project/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/eliza/306project/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/eliza/306project/devel/lib:/home/eliza/306project/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/eliza/306project/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/eliza/306project/devel/lib/pkgconfig:/home/eliza/306project/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/eliza/306project/build"
+export PYTHONPATH="/home/eliza/306project/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/eliza/306project/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/eliza/306project/src:$ROS_PACKAGE_PATH"
