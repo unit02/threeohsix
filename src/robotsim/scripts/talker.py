b3b4e9f74b27931efa69da6eca77b0b42e04671b
zall747 <zall747@aucklanduni.ac.nz>
2015-08-06 16:21:18 +1200

    Basic movement of robots in stage

--- src/robotsim/scripts/talker.py
+++ src/robotsim/scripts/talker.py
@@ -38,15 +38,23 @@
 
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    #pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        #hello_str = "hello world %s" % rospy.get_time()
+	twist = Twist()
+	twist.linear.x = 1
+	twist.angular.z = 0.01;
+	
+        rospy.loginfo(twist)
+        pub.publish(twist)
         rate.sleep()
 
 if __name__ == '__main__':
