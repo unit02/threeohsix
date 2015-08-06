f7cf1088a3935950a89bb79fdfdd41e40ab27186
chahatchawla <ccha504@aucklanduni.ac.nz>
2015-08-06 18:29:45 +1200

    added sensor to robots and static blocks

---
+++ devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/afs/ec.auckland.ac.nz/users/c/c/ccha504/unixhome/threeohsix/build/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
