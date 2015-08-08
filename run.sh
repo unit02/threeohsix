5fcc3f365f36e92e727e72543a8e4159270c569a
zall747 <zall747@aucklanduni.ac.nz>
2015-08-08 15:03:28 +1200

    fixed the running bash script

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-source devel/setup.bash
 gnome-terminal -x sh -c 'roscore'
+catkin_make
 source devel/setup.bash
 gnome-terminal -x sh -c 'roslaunch robotsim launch.launch'
 source devel/setup.bash
 gnome-terminal -x sh -c 'rosrun robotsim talker.py'
 source devel/setup.bash
 gnome-terminal -x sh -c 'rosrun stage_ros stageros src/robotsim/world/orchard.world'
+
