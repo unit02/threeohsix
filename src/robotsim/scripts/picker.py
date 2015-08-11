9f07f7c8fa2c8ff3a5ddb54eca25b4b7698cfa72
CJ <cj.park@hotmail.co.nz>
2015-08-11 16:03:47 +1200

    indentation correction

--- src/robotsim/scripts/picker.py
+++ src/robotsim/scripts/picker.py
@@ -11,78 +11,70 @@ from sensor_msgs.msg import Range
 
 
 
-class picker(node):
+class picker():
 
-	def __init__(self, name):
-		node.__init__(self, "square", 1,1, "red")
+    def __init__(self, name):
+        # node.__init__(self, "square", 1,1, "red")
+        rospy.loginfo("Starting node %s" % name)
+        # Create new topic to which it listens
+        self.laser_sub = rospy.Subscriber(
+        "/robot_3/base_scan",
+        LaserScan,
+        callback=self.laser_callback,
+        queue_size=10
+	)
+		
+        self.cmd_vel_pub = rospy.Publisher(
+            "cmd_vel",
+            Twist,
+            queue_size=10
+        )
 
-		rospy.loginfo("Starting node %s" % name)
-		self.laser_sub = rospy.Subscriber(
-			"/robot_3/base_scan",
-			LaserScan,
-			callback=self.laser_callback,
-			queue_size=10
-		)
+    def laser_callback(self,msg):
+        #Get the ranges of the laser scan and find the minimum
+        ranges = msg.ranges
+        min_distance = np.nanmin(ranges)
+        twist_msg = Twist()
+        rate = rospy.Rate(10)
+        #Avoid obstacles that were detected within 3m ahead
+        if (min_distance <= 3):
+            if (ranges.index(min_distance) <=30):
+                twist_msg.linear.x = 1
+                twist_msg.angular.z = 1
+                self.cmd_vel_pub.publish(twist_msg)
+            else:
+                twist_msg.linear.x = 1
+                twist_msg.angular.z = -1
+                self.cmd_vel_pub.publish(twist_msg)
+        else
+            # Move Straight
+            twist_msg.linear.x = 1
+            twist_msg.angular.z = 0
+            self.cmd_vel_pub.publish(twist_msg)
+        rate.sleep()
 
-		self.cmd_vel_pub = rospy.Publisher(
-			"cmd_vel",
-			Twist,
-			queue_size=10
-		)
-		pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-		rate = rospy.Rate(10) # 10hz
-
-
-	def laser_callback(self,msg):
-		ranges = msg.ranges
-		min_distance = np.nanmin(ranges)
-		twtist_msg = Twist()
-		rate = rospy.Rate(10)
-		if (min_distance <= 3):
-			if (ranges.index(min_distance) <=30):
-				twist_msg.linear.x = 1
-				twist_msg.angular.z = 1
-				self.cmd_vel_pub.publish(twist_msg)
-			else:
-				twist_msg.linear.x = 1
-				twist_msg.angular.z = -1
-				self.cmd_vel_pub.publish(twist_msg)
-		else
-			twist_msg.linear.x = 1
-			twist_msg.angular.z = 0
-			self.cmd_vel_pub.publish(twist_msg)
-		rate.sleep()
-
-        def rotateLeft:
-
-
-
-		twist = Twist()
-		twist.linear.x = 1.0
-		twist.linear.y = 0
-		twist.angular.x = 0.5
-
-
-    for i in range(100):
-        pub.publish(twist)
-        rospy.sleep(0.1) # 30*0.1 = 3.0 seconds
+    def rotateLeft:
+        twist = Twist()
+        twist.linear.x = 1.0
+        twist.linear.y = 0
+        twist.angular.x = 0.5
+        for i in range(100):
+            pub.publish(twist)
+            rospy.sleep(0.1) # 30*0.1 = 3.0 seconds
 
 
-	def rotateRight:
+    def rotateRight:
 
 		
-		
+    def pick (self):
+        pass
+        #every 1 second "pick" kiwifruit
+	#sends signal to nearest carrier to updateBin()
 
-
-	def pick (self):
-		pass
-			#every 1 second "pick" kiwifruit
-			  #sends signal to nearest carrier to updateBin()
-
-	def goThroughRow(self):
+    def goThroughRow(self):
 		pass
 
-	def goToNextRow(self):
+    def goToNextRow(self):
 		pass
 	#assumes the picker is 
 
