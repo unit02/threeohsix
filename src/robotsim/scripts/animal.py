631aaa8a31cc7cae598167a3664da1eef3c95434
CJ <cj.park@hotmail.co.nz>
2015-08-20 16:51:07 +1200

    Animal is now more random. More animalike

--- src/robotsim/scripts/animal.py
+++ src/robotsim/scripts/animal.py
@@ -4,26 +4,46 @@ from geometry_msgs.msg import Twist
 from node import node
 from random import randint 
 import threading
+import math
 
 
 class animal(node):
     #Chooses a random number between 1 and 6 and either turns or changes speed
     def random_move(self):
         twist_randmsg = Twist()
-        random_action=randint(0,6)
+        random_action=randint(0,4)
         threading.Timer(3.0, self.random_move).start()
 
         if random_action==0:
-            twist_randmsg.angular.z = 180
-            self.cmd_vel_pub.publish(twist_randmsg)
+            twist_randmsg.angular.z = math.pi
+            twist_randmsg.linear.x = randint(0,3) / 2
+            for i in range(10):
+                self.cmd_vel_pub.publish(twist_randmsg)
+                rospy.sleep(0.05)
 
         elif random_action==1:
-            twist_randmsg.angular.z = 90
-            self.cmd_vel_pub.publish(twist_randmsg)
+            twist_randmsg.angular.z = -math.pi
+            twist_randmsg.linear.x = randint(0,3) / 2
+            for i in range(10):
+                self.cmd_vel_pub.publish(twist_randmsg)
+                rospy.sleep(0.05)
 
         elif random_action==2:
-            twist_randmsg.angular.z = 270
-            self.cmd_vel_pub.publish(twist_randmsg)
+            twist_randmsg.angular.z = math.pi * 2
+            twist_randmsg.linear.x = randint(0,3) / 2
+            for i in range(10):
+                self.cmd_vel_pub.publish(twist_randmsg)
+                rospy.sleep(0.05)
+
+        elif random_action==3:
+            twist_randmsg.angular.z = -math.pi * 2
+            twist_randmsg.linear.x = randint(0,3) / 2
+            for i in range(10):
+                self.cmd_vel_pub.publish(twist_randmsg)
+                rospy.sleep(0.05)
+
+
+
 
 # The block below will be executed when the python file is executed
 # __name__ and __main__ are built-in python variables and need to start and end with *two* underscores
