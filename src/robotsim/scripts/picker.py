1b5877d6d5550bfb45f477cad313d6535cba014f
Cameron <cbhbrown@gmail.com>
2015-08-24 18:48:47 +1200

    Update picker.py
    Made picker more reliable and allowed it to go the opposite direction

--- src/robotsim/scripts/picker.py
+++ src/robotsim/scripts/picker.py
@@ -9,45 +9,40 @@ class picker(havesting_robot):
 
     def move_down_rows(self):
 	
-	x_point=self.position.x 
-	y_point=self.position.y
-        length_row=78 
-        numberofrows=2 
+	
+	row_width=6
+	row_length=78
 
-	# #for i in range(0, numberofrows):
-	# new_position = Point(x_point+length_row, y_point, 0.0)
-	# p.move_to(new_position)
-	# #p.move_to(new_position)
-	# self.detach_bin()
-	# #Move so it can safetly turn
-	# self.move_x_steps(2)
-	# new_position = Point(x_point+length_row+2, y_point-6, 0.0)
-	# p.move_to(new_position)
-	# #p.move_to(new_position)
-	# #p.reorientation()
-	# y_point=y_point-6
-	#
-	# new_position = Point(x_point+length_row-5, y_point, 0.0)
-	# p.move_to(new_position)
-	# p.wait(5)
-	# #p.move_to(new_position)
-	# #p.reorientation()
-	# new_position = Point(x_point, y_point, 0.0)
-	# p.move_to(new_position)
-	# #p.move_to(new_position)
-	# #p.reorientation()
-	# y_point=y_point-6
-
+	#For loop here with variable
+        if self.position.x < 0:
+		self.move_x_steps(row_length)
+		rospy.loginfo("Bin detaching")
+		self.detach_bin()
+		self.turnRight()
+		self.move_x_steps(row_width)
+		self.turnRight()
+		self.reorientation()
+		self.move_x_steps(8)
+		
+	else:
+		self.move_x_steps(row_length)
+		rospy.loginfo("Bin detaching")
+		self.detach_bin()
+		self.turnLeft()
+		self.move_x_steps(row_width)
+		self.turnLeft()
+		self.reorientation()
+		self.move_x_steps(8)
 
 
         #new_position = Point(x_point+length_row +3, y_point, 0.0)
-        self.move_x_steps(70)
+        '''self.move_x_steps(70)
         rospy.loginfo("Bin detaching")
         self.detach_bin()
         self.turnRight()
         self.move_x_steps(6)
         self.turnRight()
-        self.move_x_steps(70)
+        self.move_x_steps(70)'''
      #    new_position = Point(x_point+length_row + 3, y_point-5.5, 0.0)
      #    self.move_to(new_position)
      #    self.move_to(new_position)
