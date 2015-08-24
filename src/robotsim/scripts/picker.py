254f3089f1b60399eecad70befd12995a76765ff
cbro691 <cbhbrown@gmail.com>
2015-08-24 18:12:39 +1200

    Readded move to method inside the picker navigation

--- src/robotsim/scripts/picker.py
+++ src/robotsim/scripts/picker.py
@@ -9,15 +9,46 @@ from node import Face
 class picker(havesting_robot):
 
     def move_down_rows(self):
+	
+	x_point=self.position.x 
+	y_point=self.position.y
+        length_row=78 
+        numberofrows=2 
 
-        #new_position = Point(x_point+length_row +3, y_point, 0.0)
+	#for i in range(0, numberofrows): 
+	new_position = Point(x_point+length_row, y_point, 0.0) 
+	p.move_to(new_position) 
+	#p.move_to(new_position) 
+	self.detach_bin()
+	#Move so it can safetly turn 
+	self.move_x_steps(2)
+	new_position = Point(x_point+length_row+2, y_point-6, 0.0) 
+	p.move_to(new_position) 
+	#p.move_to(new_position) 
+	#p.reorientation() 
+	y_point=y_point-6 
+	 
+	new_position = Point(x_point+length_row-5, y_point, 0.0) 
+	p.move_to(new_position) 
+	p.wait(5)
+	#p.move_to(new_position) 
+	#p.reorientation() 
+	new_position = Point(x_point, y_point, 0.0) 
+	p.move_to(new_position) 
+	#p.move_to(new_position) 
+	#p.reorientation() 
+	y_point=y_point-6 
+
+
+
+        '''#new_position = Point(x_point+length_row +3, y_point, 0.0)
         self.move_x_steps(70)
         rospy.loginfo("Bin detaching")
         self.detach_bin()
         self.turnRight()
         self.move_x_steps(6)
         self.turnRight()
-        self.move_x_steps(70)
+        self.move_x_steps(70)'''
      #    new_position = Point(x_point+length_row + 3, y_point-5.5, 0.0)
      #    self.move_to(new_position)
      #    self.move_to(new_position)
