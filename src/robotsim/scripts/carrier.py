d91adf39412adf6931c4fdc1cd82ffbff7666b9d
unit02 <elizaorchard@gmail.com>
2015-08-25 22:57:01 +1200

    Fixing merge errors

--- src/robotsim/scripts/carrier.py
+++ src/robotsim/scripts/carrier.py
@@ -4,11 +4,7 @@ from geometry_msgs.msg import Point
 from havesting_robot import havesting_robot
 import roslib
 roslib.load_manifest('robotsim')
-<<<<<<< HEAD
 from robotsim.msg import bin_call,carrier_to_picker
-=======
-from robotsim.msg import bin_call
->>>>>>> d426e700b568e5f0a1e7d5cc4bece1169da19f4e
 import sys
 
 
@@ -24,8 +20,6 @@ class carrier(havesting_robot):
             callback=self._pickBin_callback,
             queue_size=1
         )
-
-<<<<<<< HEAD
 
     def _pickBin_callback(self,bin_call):
          self.wait(5)
@@ -55,24 +49,6 @@ class carrier(havesting_robot):
              self.bin_attach(bin_call.bin_name)
              #TODO turn laser back on
              self.laser_on = False
-=======
-    def _pickBin_callback(self,bin_call):
-         self.wait(5)
-         new_position = Point(bin_call.x_coordinate + 2, bin_call.y_coordinate + 2, 0.0)
-         rospy.loginfo("Moving to pick up the bin")
-         self.picking_bin = True
-
-         self.to_pick_bin_pos = new_position
-         self.move_to(new_position)
-         rospy.loginfo(self.name)
-
-         # changed so selected queue like
-         if self.name == "/robot_14":
-             self.picking_bin = False
-             self.bin_attach(bin_call.bin_name)
-         self.picking_bin = False
-         self.laser_on = True
->>>>>>> d426e700b568e5f0a1e7d5cc4bece1169da19f4e
 
     def stage_callback(self, data):
         super(carrier,self).stage_callback(data)
