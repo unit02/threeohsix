03bb734db9c5badb12ad8ea2b99a7a8ea4971093
unit02 <elizaorchard@gmail.com>
2015-08-27 10:29:09 +1200

    added one more test

--- src/robotsim/scripts/test_movement.py
+++ src/robotsim/scripts/test_movement.py
@@ -15,36 +15,45 @@ from nav_msgs.msg import Odometry
 data_from_callback = Odometry()
 
 def callback(data):
-	global data_from_callback
-	data_from_callback = data
+    global data_from_callback
+    data_from_callback = data
 
 """ This class tests the movement methods that are implemented in all the nodes"""
 class TestRobotMoves(unittest.TestCase):
-	rospy.init_node('testing_robot_moves', anonymous=True)
-	rospy.Subscriber("robot_11/odom",Odometry,callback)
+    rospy.init_node('testing_robot_moves', anonymous=True)
+    rospy.Subscriber("robot_11/odom",Odometry,callback)
 	
-	"""Test moving x meters forward method"""
-	def test_move_x_steps(self):
-		# ensure that node starts from position 0
-		self.assertEquals(int(data_from_callback.pose.pose.position.x), 0.0)
-		# move robot 5 meters using the method
-		commandX = weed.weed("robot_11", False)
-		#commandX.move_x_steps(2)
-		commandX.move_to(Point(4.0, 0.0, 0.0))
-		# testing robot is in the correct position
-		self.assertEquals(int(data_from_callback.pose.pose.position.x), 4.0)	
-				
-	def test_move_to(self):
-		# ensure that node starts from position 0
-		self.assertEquals(int(data_from_callback.pose.pose.position.x), 0.0)
-		# move robot 5 meters using the method
-		commandX = weed.weed("Robot_11", False)
-		commandX.move_x_steps(10)
-		# testing robot is in the correct position
-		self.assertEquals(int(data_from_callback.pose.pose.position.x), 3.0)
-		#self.assertEquals(int(data_from_callback.pose.pose.position.y), 0.0)
-		#self.assertEquals(int(data_from_callback.pose.pose.position.z), 0.0)
+    """Test move_to_method"""
+    def test_move_to(self):
+        # ensure that node starts from position 0
+        self.assertEquals(int(data_from_callback.pose.pose.position.x), 0.0)
+        # move robot to position (4,0,0)
+        commandX = weed.weed("robot_11", False)
+        commandX.move_to(Point(4.0, 0.0, 0.0))
+        # testing robot is in the correct position
+        self.assertEquals(int(data_from_callback.pose.pose.position.x), 4.0)
+
+    def test_move_to_backwards(self):
+        # ensure that node starts from the earliest position (4,0,0)
+        self.assertEquals(int(data_from_callback.pose.pose.position.x), 4.0)
+        # move robot backward to position (-4,0,0)
+        commandX = weed.weed("robot_11", False)
+        commandX.move_to(Point(-4.0, 0.0, 0.0))
+        # testing robot is in the correct position
+        self.assertEquals(int(data_from_callback.pose.pose.position.x), -4.0)
+
+
+    # def move_x_steps(self):
+    #     # ensure that node starts from position 0
+    #     self.assertEquals(int(data_from_callback.pose.pose.position.x), 0.0)
+    #     # move robot 5 meters using the method
+    #     commandX = weed.weed("robot_19", False)
+    #     commandX.move_x_steps(5)
+    #     # testing robot is in the correct position
+    #     self.assertEquals(int(data_from_callback.pose.pose.position.x), 5.0)
+    #     #self.assertEquals(int(data_from_callback.pose.pose.position.y), 0.0)
+		# #self.assertEquals(int(data_from_callback.pose.pose.position.z), 0.0)
 
 if __name__ == '__main__':
-	import rostest
-	rostest.rosrun(PKG, 'test_robot_moves', TestRobotMoves)
+    import rostest
+    rostest.rosrun(PKG, 'test_robot_moves', TestRobotMoves)
